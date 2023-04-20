b0aa640b7d816f2e7595e8c017ea57506eae10a8
SHAHINSAH KUMAR <112196203+SHAHINSAH@users.noreply.github.com>
2023-04-20 23:36:55 +0530

    h

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
 //Middlewares
 app.use(express.json());
 app.get("/api/v1/products/:id", (req, res)=>{
-    const product = products.find((el)=> el.id === req.params.id);
+    const product = products.find((el)=> el.id ===Number(req.params.id));
     if(!product){
         return res.status(404).json({
             status: "fail",
